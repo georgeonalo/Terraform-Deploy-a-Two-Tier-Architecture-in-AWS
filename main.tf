ed0bce16c60c8bc38f0d53ca3ccf843a6f3577c3
George Onalo <Onalotech7@gmail.com>
2023-05-12 13:08:33 +0100

    create files

---
+++ main.tf
@@ -0,0 +1,14 @@
+# Configure provider
+provider "aws" {
+  region  = "us-east-1"
+  profile = "terraform-user"
+}
+
+# store terraform state file in s3
+terraform {
+  backend "s3" {
+    bucket = "george-terraform-remote-state"
+    key    = "terraform.tfstate.dev"
+    region = "us-east-1"
+  }
+}
